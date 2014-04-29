efdf217eb3dfd4f35ba8b809f08908cfa9197a4e
DavidVaz <vazdavid@icloud.com>
2014-04-29 22:31:12 +0200

    Delete devices from here

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-add_lunch_combo grouper-user
-add_lunch_combo tilapia-user
-add_lunch_combo hammerhead-user
-add_lunch_combo mako-user
-add_lunch_combo manta-user
+#add_lunch_combo grouper-user
+#add_lunch_combo tilapia-user
+#add_lunch_combo hammerhead-user
+#add_lunch_combo mako-user
+#add_lunch_combo manta-user
